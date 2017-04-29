730b0967e935db122ff1bab246f3b9f069250bdd
Keeley Erhardt <keeley.erhardt@gmail.com>
2017-04-29 15:30:31 -0400

    Clean up unused code

--- js/main.js
+++ js/main.js
@@ -9,17 +9,6 @@ var $btns = $('.btn').click(function() {
   $(this).addClass('active');
 })
 
-$('.range-slider').jRange({
-    from: 0,
-    to: 100,
-    step: 1,
-    scale: [0,25,50,75,100],
-    format: '%s',
-    width: 300,
-    showLabels: true,
-    isRange : true
-});
-
 function bugPage() {
   window.location.href = "bugPage.html";
 };
