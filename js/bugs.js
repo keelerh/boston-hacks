2ab47552a654c1d754ba0a31cbc5b356b1c31cad
Tamar Weseley <tweseley@gmail.com>
2017-04-29 15:07:16 -0400

    adding new bootstrap template

---
+++ js/bugs.js
@@ -0,0 +1,18 @@
+// template:
+//bug = {username: "", title: "", compensation: "", language: "", twitterDesc: "", fullDesc: "", code: ""};
+
+twilioBug = {username: "Tamar Weseley", title: "Twilio Help!", compensation: "500",
+  language: "JavaScript",
+  twitterDesc: "Issue when using Twilio API - can't get text messages to go through.",
+  fullDesc: "MEEP MEEP MEEP",
+  code: "this is my code"
+};
+
+dbBug = {username: "Keeley Erhardt", title: "Database Help!", compensation: "100",
+  language: "Python",
+  twitterDesc: "Can't figure out how to initialize my database.",
+  fullDesc: "MEEP MEEP MEEP",
+  code: "this is my code"
+};
+
+bugs = [twilioBug, dbBug];
